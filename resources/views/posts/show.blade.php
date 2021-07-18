0c4f4678110f63d6557a0cc81c550ff026987400
Adellya Winarni <adellyawinarni@gmail.com>
2021-07-19 06:30:22 +0700

    update 20210719

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 
     <div class="container py-5">
         <h2>{{ $post->title }}</h2>
-        <p>Posted on <strong>{{ $post->category->name }}</strong> at {{ $post->created_at}}</p>
+        <p>Posted on <strong>{{ $post->category->name }}</strong> at {{ $post->created_at->format('j F Y g:i a')}}</p>
         <p>{!! $post->text !!}</p>
     </div>
 
